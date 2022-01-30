460536f9ffd6337a62fc03fc3177bbfefb41837c
Dan Maffia <73266204+dan998@users.noreply.github.com>
2022-01-30 19:08:47 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,76 @@
+
+# Termux-Games
+# Coded by: Dan Maffia 
+# Github: https://github.com/dan998
+
+
+
+echo -e "\e[032m" "Please Don't copy the Code And Give creadits to the Coding Family"
+
+
+clear
+
+
+echo "play Games in termux pleaee enjoy"
+
+echo -e "\e[032m"
+pkg install ruby -y && gem install lolcat
+pkg install figlet
+
+figlet bastet | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install bastet
+
+figlet Pacman | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install pacman4console
+
+figlet M-buggy | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install moon-buggy
+
+figlet invaders | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install ninvaders
+
+figlet snake | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install nsnake
+
+figlet Greed | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install greed
+
+figlet Nethack | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install nethack
+
+figlet Sudoku | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install nudoku && apt install nudoku
+
+figlet Hangman | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install git -y && echo -e "\e[032m" && git clone https://github.com/khansaad1275/HangmanPy.git
+
+figlet Python | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install python -y
+
+figlet "2048" | lolcat && echo Installing..................... | lolcat
+echo -e "\e[032m"
+pkg install git -y && pkg install wget -y && pkg install clang -y && wget https://raw.githubusercontent.com/mevdschee/2048.c/master/2048.c && sleep 2 && gcc -o 2048 2048.c
+
+cd && echo "if [ -x /data/data/com.termux/files/usr/libexec/termux/command-not-found ]; then
+         command_not_found_handle() {
+                 /data/data/com.termux/files/usr/libexec/termux/command-not-found "$1"
+         }
+ fi
+
+ PS1='$'
+alias games='cd && cd Codersgames && bash games.sh'" > /data/data/com.termux/files/usr/etc/bash.bashrc
+
+echo "Thanks for supporting the Coding Family" | lolcat -a
+echo ""
+echo -e '\033[1mType ./game.sh to start the Termux-Games\033[0m' | lolcat -a
+echo -e '\033[1mAfter Restaring the Termux you can Type games in termux to Directly start the Games Menu.\033[0m' | lolcat -a
